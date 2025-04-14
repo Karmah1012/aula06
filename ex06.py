d92fe3225b9336cf60f16010f9623721e6f8142d
Karmah <karmah1012@gmail.com>
2025-04-14 17:31:34 -0300

    finalizado o ultimo exercicio

--- ex06.py
+++ ex06.py
@@ -1,11 +1,14 @@
-pa = float(input("Digite a primeira nota: "))
-while pa < 0  or pa > 10 :
-    pa = float(input("Valor fora da faixa, \n"
-                     "Digite a nota1 novamente:"))
-pu = float(input("Digite uma nota2 : "))
-while pu > 10 or pu < 0 :
-    pu = float(input("Valor2 fora da faixa:\n"
-                     "Digite o valor 2 novamente: "))
-media = (pa + pu) / 2
-print(media)
+novocalculo = "s"
+while novocalculo == "s" :
+    pa = float(input("Digite a primeira nota: "))
+    while pa < 0  or pa > 10 :
+        pa = float(input("Valor fora da faixa, \n"
+                         "Digite a nota1 novamente:"))
+    pu = float(input("Digite uma nota2 : "))
+    while pu > 10 or pu < 0 :
+        pu = float(input("Valor2 fora da faixa:\n"
+                         "Digite o valor 2 novamente: "))
+    media = (pa + pu) / 2
+    print(media)
+    novocalculo = input("Deseja repetir o processo? ")
 
